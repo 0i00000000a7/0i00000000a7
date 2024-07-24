805b66bb73f29db8dfd08999aa9c5cb116bfa863
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2024-07-24 17:09:17 +0800

    Update formatBE.js

--- formatBE.js
+++ formatBE.js
@@ -1,13 +1,13 @@
-function formatBE(ex, acc) {
+function formatBB(ex, acc) {
         let e = ex.log10().floor()
 		if (ex.lt(1e6)) {
 			return formatShort(ex, acc)
 		} else if (ex.gte("eeee10")) {
 			let slog = ex.slog()
-			return (slog.gte(1e9)?'':E(10).pow(slog.sub(slog.floor())).toFixed(3)) + "F" + format(slog.floor(), 0)
+			return (slog.gte(1e9)?'':BE(10).pow(slog.sub(slog.floor())).toFixed(3)) + "F" + format(slog.floor(), 0)
 		} else {
-			let m = ex.div(E(10).pow(e))
-			let a = E(4).sub(e.log10().floor())
+			let m = ex.div(BE(10).pow(e))
+			let a = BE(4).sub(e.log10().floor())
 			return (a.lt(0)?'':m.toFixed(a))+'e'+format(e, 0)
 		}
 }
